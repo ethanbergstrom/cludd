966cdb323e0a64682e55875808f5cac9fb8843f6
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-19 19:23:51 +0000

    Formatting

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 resource random_string base {
   length  = 5
-  numeric  = false
+  numeric = false
   special = false
-  upper = false
+  upper   = false
 }
 
 resource oci_identity_compartment base {
   compartment_id = var.compartment_ocid
-  name = random_string.base.result
-  description = var.stack_compartment_description
+  name           = random_string.base.result
+  description    = var.stack_compartment_description
 }
 
 resource oci_ons_notification_topic base {
   compartment_id = oci_identity_compartment.base.id
-  name = random_string.base.result
+  name           = random_string.base.result
 }
 
 resource oci_devops_project base {
   compartment_id = oci_identity_compartment.base.id
-  name = random_string.base.result
+  name           = random_string.base.result
 
   notification_config {
     topic_id = oci_ons_notification_topic.base.id
@@ -27,13 +27,13 @@ resource oci_devops_project base {
 
 resource oci_kms_vault base {
   compartment_id = oci_identity_compartment.base.id
-  display_name = random_string.base.result
-  vault_type = "DEFAULT"
+  display_name   = random_string.base.result
+  vault_type     = "DEFAULT"
 }
 
 resource oci_kms_key base {
   compartment_id      = oci_identity_compartment.base.id
-  display_name = random_string.base.result
+  display_name        = random_string.base.result
   management_endpoint = oci_kms_vault.base.management_endpoint
 
   key_shape {
@@ -44,13 +44,14 @@ resource oci_kms_key base {
 
 resource oci_vault_secret base {
   compartment_id = oci_identity_compartment.base.id
-  key_id = oci_kms_key.base.id
   secret_name    = random_string.base.result
+  key_id         = oci_kms_key.base.id
   vault_id       = oci_kms_vault.base.id
+
   secret_content {
     # Base64 is the only option
     content_type = "BASE64"
-    content = base64encode(var.github_token)
+    content      = base64encode(var.github_token)
   }
   
   lifecycle {
@@ -59,17 +60,17 @@ resource oci_vault_secret base {
 }
 
 resource random_string put_repo {
-  length  = 5
+  length   = 5
   numeric  = false
-  special = false
-  upper = false
+  special  = false
+  upper    = false
 }
 
 resource random_string get_repo {
-  length  = 5
+  length   = 5
   numeric  = false
-  special = false
-  upper = false
+  special  = false
+  upper    = false
 }
 
 resource oci_artifacts_container_repository base {
@@ -94,16 +95,16 @@ locals {
 resource oci_devops_deploy_artifact base {
   for_each = local.repo_uris
 
+  project_id                 = oci_devops_project.base.id
+  display_name               = each.key
   argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
-  deploy_artifact_type = "DOCKER_IMAGE"
-  display_name         = each.key
-  project_id = oci_devops_project.base.id
+  deploy_artifact_type       = "DOCKER_IMAGE"
 
   deploy_artifact_source {
     deploy_artifact_source_type = "OCIR"
-    image_digest = ""
+    image_digest                = ""
     ## The $$ in Terraform will let us use ${imageVersion} as a literal string and wont try to interpolte it into a Terraform variable, so that OCI can use it as a variable
-    image_uri    = "${each.value}:$${imageVersion}"
+    image_uri                   = "${each.value}:$${imageVersion}"
   }
 }
 
@@ -112,16 +113,16 @@ resource oci_devops_build_pipeline base {
 }
 
 resource oci_devops_connection base {
-  project_id = oci_devops_project.base.id
-  access_token = oci_vault_secret.base.id
+  project_id      = oci_devops_project.base.id
+  access_token    = oci_vault_secret.base.id
   connection_type = "GITHUB_ACCESS_TOKEN"
 }
 
 resource oci_devops_build_pipeline_stage build {
-  build_pipeline_id = oci_devops_build_pipeline.base.id
+  build_pipeline_id         = oci_devops_build_pipeline.base.id
   build_pipeline_stage_type = "BUILD"
-  image = "OL7_X86_64_STANDARD_10"
-  primary_build_source = "SourceRepo"
+  image                     = "OL7_X86_64_STANDARD_10"
+  primary_build_source      = "SourceRepo"
 
   build_pipeline_stage_predecessor_collection {
     items {
@@ -139,13 +140,13 @@ resource oci_devops_build_pipeline_stage build {
       connection_id   = oci_devops_connection.base.id
       connection_type = "GITHUB"
       name            = "SourceRepo"
-      repository_url = var.github_url
+      repository_url  = var.github_url
     }
   }
 }
 
 resource oci_devops_build_pipeline_stage deliver {
-  build_pipeline_id = oci_devops_build_pipeline.base.id
+  build_pipeline_id         = oci_devops_build_pipeline.base.id
   build_pipeline_stage_type = "DELIVER_ARTIFACT"
 
   build_pipeline_stage_predecessor_collection {
@@ -168,20 +169,20 @@ resource oci_devops_build_pipeline_stage deliver {
 
 resource oci_logging_log_group devops {
   compartment_id = oci_identity_compartment.base.id
-  display_name = "DevOps"
+  display_name   = "DevOps"
 }
 
 resource oci_logging_log devops {
-  display_name = "Operations"
-  is_enabled         = "true"
   log_group_id       = oci_logging_log_group.devops.id
+  display_name       = "Operations"
+  is_enabled         = "true"
   log_type           = "SERVICE"
   retention_duration = "30"
 
   configuration {
     source {
-      category    = "all"
       resource    = oci_devops_project.base.id
+      category    = "all"
       service     = "devops"
       source_type = "OCISERVICE"
     }
@@ -189,25 +190,24 @@ resource oci_logging_log devops {
 }
 
 resource random_string devops {
-  length  = 5
+  length   = 5
   numeric  = false
-  special = false
-  upper = false
+  special  = false
+  upper    = false
 }
 
 resource oci_identity_dynamic_group devops {
   compartment_id = var.tenancy_ocid
   name           = random_string.devops.result
   description    = "DevOps resource identities"
-  matching_rule = "All {resource.compartment.id = '${oci_identity_compartment.base.id}', Any {resource.type = 'devopsdeploypipeline', resource.type = 'devopsbuildpipeline', resource.type = 'devopsrepository', resource.type = 'devopsconnection', resource.type = 'devopstrigger'}}"
+  matching_rule  = "All {resource.compartment.id = '${oci_identity_compartment.base.id}', Any {resource.type = 'devopsdeploypipeline', resource.type = 'devopsbuildpipeline', resource.type = 'devopsrepository', resource.type = 'devopsconnection', resource.type = 'devopstrigger'}}"
 }
 
 resource oci_identity_policy devops {
   compartment_id = oci_identity_compartment.base.id
   name           = random_string.devops.result
   description    = "DevOps to complete its pipeline steps"
-
-  statements = [
+  statements     = [
     "Allow dynamic-group id ${oci_identity_dynamic_group.devops.id} to manage devops-family in compartment id ${oci_identity_compartment.base.id}",
     "Allow dynamic-group id ${oci_identity_dynamic_group.devops.id} to manage functions-family in compartment id ${oci_identity_compartment.base.id}",
     "Allow dynamic-group id ${oci_identity_dynamic_group.devops.id} to manage generic-artifacts in compartment id ${oci_identity_compartment.base.id}",
@@ -221,14 +221,18 @@ resource oci_identity_policy devops {
 resource oci_devops_build_run base {
   build_pipeline_id = oci_devops_build_pipeline.base.id
   # Ensure it runs after DeliverArtifact stage is in place, Logging is enabled, and necessarily permissions are granted
-  depends_on = [oci_logging_log.devops,oci_devops_build_pipeline_stage.deliver,oci_identity_policy.devops]
+  depends_on        = [
+    oci_logging_log.devops,
+    ci_devops_build_pipeline_stage.deliver,
+    oci_identity_policy.devops
+  ]
 }
 
 module fn {
   source = "./modules/fn"
   tenancy_ocid = var.tenancy_ocid
   compartment_ocid = oci_identity_compartment.base.id
-  image_uris = {
+  image_uris       = {
     # Use this loop (instead of the build run artifact URIs) to maintain the mapping of function name to image
     for k, v in local.repo_uris : k => "${v}:${oci_devops_build_run.base.build_outputs[0].exported_variables[0].items[0].value}"
   }
@@ -237,10 +241,10 @@ module fn {
 resource oci_devops_deploy_environment base {
   for_each = module.fn.function_ids
 
-  deploy_environment_type = "FUNCTION"
+  project_id              = oci_devops_project.base.id
   display_name            = each.key
-  function_id = each.value
-  project_id = oci_devops_project.base.id
+  deploy_environment_type = "FUNCTION"
+  function_id             = each.value
 }
 
 resource oci_devops_deploy_pipeline base {
@@ -250,10 +254,10 @@ resource oci_devops_deploy_pipeline base {
 resource oci_devops_deploy_stage base {
   for_each = oci_devops_deploy_environment.base
 
-  deploy_pipeline_id = oci_devops_deploy_pipeline.base.id
-  function_deploy_environment_id = each.value.id
-  deploy_stage_type = "DEPLOY_FUNCTION"
+  deploy_pipeline_id              = oci_devops_deploy_pipeline.base.id
   display_name                    = each.key
+  function_deploy_environment_id  = each.value.id
+  deploy_stage_type               = "DEPLOY_FUNCTION"
   docker_image_deploy_artifact_id = oci_devops_deploy_artifact.base[each.key].id
   
   deploy_stage_predecessor_collection {
@@ -265,9 +269,9 @@ resource oci_devops_deploy_stage base {
 
 # Append the Triger Deploy build step to the Build pipeline
 resource oci_devops_build_pipeline_stage base {
-  build_pipeline_id = oci_devops_build_pipeline.base.id
-  build_pipeline_stage_type = "TRIGGER_DEPLOYMENT_PIPELINE"
-  deploy_pipeline_id = oci_devops_deploy_pipeline.base.id
+  build_pipeline_id              = oci_devops_build_pipeline.base.id
+  build_pipeline_stage_type      = "TRIGGER_DEPLOYMENT_PIPELINE"
+  deploy_pipeline_id             = oci_devops_deploy_pipeline.base.id
   is_pass_all_parameters_enabled = "true"
   
   build_pipeline_stage_predecessor_collection {
@@ -275,6 +279,7 @@ resource oci_devops_build_pipeline_stage base {
       id = oci_devops_build_pipeline_stage.deliver.id
     }
   }
+
   # Don't append the trigger step until the Deploy pipeline is fully built
   depends_on = [oci_devops_deploy_stage.base]
 }
--- modules/fn/modules/core/main.tf
+++ modules/fn/modules/core/main.tf
@@ -1,49 +1,51 @@
 resource oci_core_vcn base {
   compartment_id = var.compartment_ocid
-  cidr_blocks = [var.vcn_cidr_block]
+  cidr_blocks    = [var.vcn_cidr_block]
 }
 
 resource oci_core_network_security_group base {
   compartment_id = var.compartment_ocid
-  vcn_id = oci_core_vcn.base.id
+  vcn_id         = oci_core_vcn.base.id
 }
 
 resource oci_core_internet_gateway base {
   compartment_id = var.compartment_ocid
-  vcn_id = oci_core_vcn.base.id
+  vcn_id         = oci_core_vcn.base.id
 }
 
 resource oci_core_default_route_table base {
   manage_default_resource_id = oci_core_vcn.base.default_route_table_id
 
   route_rules {
-	  destination = "0.0.0.0/0"
+	  destination       = "0.0.0.0/0"
 	  network_entity_id = oci_core_internet_gateway.base.id
   }
 }
 
 resource oci_core_security_list https {
   compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.base.id
+
   egress_security_rules {
     destination      = "0.0.0.0/0"
-    protocol  = "6"
+    protocol         = "6"
   }
   ingress_security_rules {
     protocol    = "6"
     source      = "0.0.0.0/0"
+
     tcp_options {
       max = "443"
       min = "443"
     }
   }
-  vcn_id = oci_core_vcn.base.id
 }
 
 resource oci_core_subnet base {
-  compartment_id = var.compartment_ocid
-  vcn_id = oci_core_vcn.base.id
+  compartment_id    = var.compartment_ocid
+  vcn_id            = oci_core_vcn.base.id
   # Use the entire VCN
-  cidr_block = var.vcn_cidr_block
+  cidr_block        = var.vcn_cidr_block
   security_list_ids = [
     oci_core_security_list.https.id
   ]
