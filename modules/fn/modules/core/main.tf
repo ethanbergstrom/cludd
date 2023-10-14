cc4c8cf0ca0e409dfe58542dd136fae6e89cd297
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-10-14 16:41:48 +0000

    No longer use security lists

--- modules/fn/modules/core/main.tf
+++ modules/fn/modules/core/main.tf
@@ -37,34 +37,11 @@ resource oci_core_default_route_table base {
   }
 }
 
-resource oci_core_security_list https {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.base.id
-
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "6"
-  }
-
-  ingress_security_rules {
-    source   = "0.0.0.0/0"
-    protocol = "6"
-
-    tcp_options {
-      max = "443"
-      min = "443"
-    }
-  }
-}
-
 resource oci_core_subnet base {
   compartment_id    = var.compartment_ocid
   vcn_id            = oci_core_vcn.base.id
   # Use the entire VCN
   cidr_block        = var.vcn_cidr_block
-  security_list_ids = [
-    oci_core_security_list.https.id
-  ]
 }
 
 output subnet_id {
